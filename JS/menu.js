a54939ede2205506010ef49ccf569feac790b57a
FlorLizardo <florencializardo7@gmail.com>
2023-08-22 17:31:34 +0200

    feat: Finish mobile

---
+++ JS/menu.js
@@ -0,0 +1,15 @@
+const openMenu = document.querySelector('#open-menu');
+const closeMenu = document.querySelector('#close-menu');
+const aside = document.querySelector('aside');
+
+openMenu.addEventListener('click', () => {
+ aside.classList.add('aside-visible');
+});
+
+closeMenu.addEventListener('click', () => {
+ aside.classList.remove('aside-visible');
+});
+
+bottoniCategorie.forEach(bottone => bottone.addEventListener('click', () => {
+  aside.classList.remove('aside-visible');
+}));
